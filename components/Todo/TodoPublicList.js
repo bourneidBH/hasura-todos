2b62082a1437ddb9633fd7581c4087de3b33128c
bourneidBH <mbourne@findmymobilehome.com>
2022-12-05 14:36:17 -0600

    handle public todos

--- components/Todo/TodoPublicList.js
+++ components/Todo/TodoPublicList.js
@@ -1,51 +1,157 @@
-import React, { Fragment } from "react";
+import React, { useState, useEffect, Fragment } from "react";
+import { gql, useSubscription, useApolloClient } from "@apollo/client";
 
 import TaskItem from "./TaskItem";
 
-const TodoPublicList = props => {
-  const state = {
-    olderTodosAvailable: true,
-    newTodosCount: 1,
-    todos: [
-      {
-        id: "1",
-        title: "This is public todo 1",
-        user: {
-          name: "someUser1"
-        }
+const NOTIFY_NEW_PUBLIC_TODOS = gql`
+  subscription notifyNewPublicTodos {
+    todos (
+      where: {
+        is_public: { _eq: true }
       },
-      {
-        id: "2",
-        title: "This is public todo 2",
-        is_completed: false,
-        is_public: true,
-        user: {
-          name: "someUser2"
-        }
+      limit: 1,
+      order_by: { created_at: desc }
+    ) {
+      id
+      created_at
+    }
+  }
+`
+
+const GET_OLD_PUBLIC_TODOS = gql`
+  query getOldPublicTodos ($oldestTodoId: Int!) {
+    todos (
+      where: {
+        is_public: { _eq: true },
+        id: { _lt: $oldestTodoId }
       },
-      {
-        id: "3",
-        title: "This is public todo 3",
-        user: {
-          name: "someUser3"
-        }
-      },
-      {
-        id: "4",
-        title: "This is public todo 4",
-        user: {
-          name: "someUser4"
-        }
+      limit: 7,
+      order_by: { created_at: desc }
+    ) {
+      id
+      title
+      created_at
+      user {
+        name
       }
-    ]
+    }
+  }
+`
+
+const GET_NEW_PUBLIC_TODOS = gql`
+  query getNewPublicTodos ($latestVisibleId: Int!) {
+    todos (
+      where: {
+        is_public: { _eq: true },
+        id: { _gt: $latestVisibleId }
+      },
+      order_by: { created_at: desc }
+    ) {
+      id
+      title
+      created_at
+      user {
+        name
+      }
+    }
+  }
+`
+
+const TodoPublicList = ({ latestTodo }) => {
+  const client = useApolloClient()
+  const [state, setState] = useState({
+    olderTodosAvailable: latestTodo ? true : false,
+    newTodosCount: 0,
+    todos: [],
+    error: false
+  })
+
+  let numTodos = state.todos.length;
+  let oldestTodoId = numTodos
+    ? state.todos[numTodos - 1].id
+    : latestTodo
+      ? latestTodo.id + 1
+      : 0;
+
+  let newestTodoId = numTodos
+    ? state.todos[0].id
+    : latestTodo
+      ? latestTodo.id
+      : 0;
+
+  useEffect(() => {
+    loadOlder()
+  }, [])
+
+  useEffect(() => {
+    if (latestTodo && latestTodo.id > newestTodoId) {
+      setState({
+        ...state,
+        newTodosCount: state.newTodosCount + 1
+      })
+      newestTodoId = latestTodo.id
+    }
+  }, [latestTodo])
+
+  const loadNew = async () => {
+    const { error, data } = await client.query({
+      query: GET_NEW_PUBLIC_TODOS,
+      variables: { latestVisibleId: state.todos.length ? state.todos[0].id : null }
+    })
+
+    if (data) {
+      setState({
+        ...state,
+        todos: [
+          ...data.todos,
+          ...state.todos,
+        ],
+        newTodosCount: 0
+      })
+      newestTodoId = data.todos[0].id
+    }
+
+    if (error) {
+      console.log(error)
+      setState({
+        ...state,
+        error: true
+      })
+    }
   };
 
-  const loadNew = () => {};
+  const loadOlder = async () => {
+    const { error, data } = await client.query({
+      query: GET_OLD_PUBLIC_TODOS,
+      variables: { oldestTodoId: oldestTodoId }
+    })
+    if (data?.todos?.length) {
+      setState({
+        ...state,
+        todos: [
+          ...state.todos,
+          ...data.todos
+        ]
+      })
+      oldestTodoId = data.todos[data.todos.length - 1].id
+    } else {
+      setState({
+        ...state,
+        olderTodosAvailable: false
+      })
+    }
 
-  const loadOlder = () => {};
+    if (error) {
+      console.log(error)
+      setState({
+        ...state,
+        error: true
+      })
+    }
+  };
 
   let todos = state.todos;
-
+  
   const todoList = (
     <ul>
       {todos.map((todo, index) => {
@@ -82,4 +188,17 @@ const TodoPublicList = props => {
   );
 };
 
-export default TodoPublicList;
+const TodoPublicListSubscription = () => {
+  const { loading, error, data } = useSubscription(NOTIFY_NEW_PUBLIC_TODOS)
+
+  if (loading) return <span>Loading...</span>
+  if (error) {
+    console.log(error)
+    return <span>Error!</span>
+  }
+  return (
+    <TodoPublicList latestTodo={data?.todos?.length ? data.todos[0] : null} />
+  )
+}
+
+export default TodoPublicListSubscription;
